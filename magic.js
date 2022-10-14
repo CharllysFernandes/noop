847d1e3b957346a360c9385ea1be96e67b61067a
Charllys Fernandes <charllys.daiany@gmail.com>
2022-10-14 14:31:16 -0300

    code review

--- magic.js
+++ magic.js
@@ -1,15 +1,16 @@
-
 function option1() {
     if (document.getElementById("check2").checked == true) {
         document.getElementById("check3").checked = false
     }
 }
+
 function option2() {
     if (document.getElementById("check3").checked == true) {
         document.getElementById("check1").checked = false
     }
 
 }
+
 function option3() {
     if (document.getElementById('check1').checked == true) {
         document.getElementById('check2').checked = false
@@ -44,30 +45,30 @@ if (array_options.length == 3) {
 
 // Pega valor da opção 1 para adicionar a div
 function mudarTexto1() {
-   
+
     label_opcao = document.getElementById("input1").value
     document.getElementById('label_option1').innerHTML = label_opcao
- 
- 
-console.log(label_opcao)
+
+
+    console.log(label_opcao)
 }
 
 // Pega valor da opção 2 para adicionar a div
 function mudarTexto2() {
     label_opcao = document.getElementById("input2").value
     document.getElementById('label_option2').innerHTML = label_opcao
-   
-    
-console.log(label_opcao)
+
+
+    console.log(label_opcao)
 }
 
 // Pega valor da opção 1 para adicionar a div
 function mudarTexto3() {
     label_opcao = document.getElementById("input3").value
     document.getElementById('label_option3').innerHTML = label_opcao
-    
-    
-console.log(label_opcao)
+
+
+    console.log(label_opcao)
 }
 
 // Copiar para area de transferencia
@@ -82,12 +83,12 @@ function copiarURL() {
     // split join pra remover espaços no url
     copyUrl = window.location.href + "?" + option1.split(" ").join("") + "+" + option2.split(" ").join("") + "+" + option3.split(" ").join("") + "+"
 
-   if (option1 == ""| option2 == "" | option3 == "") {
-    alert("Sorry! empty input!")
-   } else {
-    navigator.clipboard.writeText(copyUrl)
-    alert("Copy to clipboard: "+copyUrl)
-    
-   }
+    if (option1 == "" | option2 == "" | option3 == "") {
+        alert("Sorry! empty input!")
+    } else {
+        navigator.clipboard.writeText(copyUrl)
+        alert("Copy to clipboard: " + copyUrl)
+
+    }
 
 }
