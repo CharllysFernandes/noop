8b5eeadf2124a73260fb16bd43872cb4fae2afb7
Charllys Fernandes <charllys.daiany@gmail.com>
2023-02-13 11:13:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const check1 = document.getElementById('switch1');
 const check2 = document.getElementById('switch2')
 const check3 = document.getElementById('switch3')
 
+
 check1.addEventListener('click', function () {
-    check2.setAttribute("checked", "")
-    check3.setAttribute("checked", "")
+    changeSwitch(check1)
 
 })
+check2.addEventListener('click', function () {
+    changeSwitch(check2)
+})
+check3.addEventListener('click', function () {
+    changeSwitch(check1)
+    changeSwitch(check3)
+})
 
-function changeSwitch(check, status ) {
-    // use hasAttributes
-        
+function changeSwitch(check) {
+    if (check.hasAttribute("checked")) {
+        check.removeAttribute("checked")
+    } else {
+        check.setAttribute("checked", "")
+    }
 }
