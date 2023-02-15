9637044741814827495019d57138de494d2d0e03
Charllys Fernandes <charllys.daiany@gmail.com>
2023-02-15 11:19:39 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const check1 = document.getElementById('switch1');
-const check2 = document.getElementById('switch2')
-const check3 = document.getElementById('switch3')
+const check0 = document.getElementById('chck1')
+const check1 = document.getElementById('chck2')
+const check2 = document.getElementById('chck3')
 
+const check = (n) => document.getElementById(`check${n}`)
 
-check1.addEventListener('click', function () {
-    changeSwitch(check1)
+const save = (data) => localStorage.setItem('noop', JSON.stringify(data))
+const getSave = localStorage.getItem('noop')
+let arr = new Array()
 
-})
-check2.addEventListener('click', function () {
-    changeSwitch(check2)
-})
-check3.addEventListener('click', function () {
-    changeSwitch(check1)
-    changeSwitch(check3)
-})
+// arr.push(JSON.parse(getSave))
 
-function changeSwitch(check) {
-    if (check.hasAttribute("checked")) {
-        check.removeAttribute("checked")
-    } else {
-        check.setAttribute("checked", "")
+arr = JSON.parse(getSave)
+
+console.log(getSave)
+render(arr)
+
+function addArray() {
+    newArray =  {check: true }
+    arr.push(newArray)
+    console.log(arr)
+    save(arr)
+}
+
+function render(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].check === true) {
+            console.log(i+1)
+            check(i+1).setAttribute("checked", "")
+            console.log(check[i])
+        }
+
     }
 }
