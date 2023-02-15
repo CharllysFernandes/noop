e1c1b2d27e808849740465165501c90cb5d4804c
Charllys Fernandes <charllys.daiany@gmail.com>
2023-02-15 12:07:09 -0300

    update index.html

--- magic.js
+++ magic.js
@@ -33,46 +33,15 @@ console.log(array_options)
 
 if (array_options.length == 3) {
     console.log('tem 3')
-    document.getElementById('section-card').style.visibility = 'hidden';
-    document.getElementById('label_option1').innerHTML = array_options[0]
-    document.getElementById('label_option2').innerHTML = array_options[1]
-    document.getElementById('label_option3').innerHTML = array_options[2]
+    document.getElementById('input1').value = array_options[0]
+    document.getElementById('input2').value = array_options[1]
+    document.getElementById('input3').value = array_options[2]
 
-} else {
-    console.log('menos 3')
-    document.getElementById('section-card').style.visibility = 'visible';
-}
-
-// Pega valor da opção 1 para adicionar a div
-function mudarTexto1() {
-
-    label_opcao = document.getElementById("input1").value
-    document.getElementById('label_option1').innerHTML = label_opcao
+} 
 
 
-    console.log(label_opcao)
-}
-
-// Pega valor da opção 2 para adicionar a div
-function mudarTexto2() {
-    label_opcao = document.getElementById("input2").value
-    document.getElementById('label_option2').innerHTML = label_opcao
-
-
-    console.log(label_opcao)
-}
-
-// Pega valor da opção 1 para adicionar a div
-function mudarTexto3() {
-    label_opcao = document.getElementById("input3").value
-    document.getElementById('label_option3').innerHTML = label_opcao
-
-
-    console.log(label_opcao)
-}
 
 // Copiar para area de transferencia
-
 
 
 function copiarURL() {
